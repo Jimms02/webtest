8acceea6437007309c94e10927771b3c71d55a82
jimm <jimmsaw2002@gmail.com>
2025-03-01 10:36:19 +0800

    form

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import styles from "./page.module.css";
+import TodoForm from "../components/todoForm";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
+      <TodoForm/>
     </div>
   );
 }
